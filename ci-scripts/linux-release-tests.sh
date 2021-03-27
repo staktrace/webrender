681f6ba7af3a03a0b0825d7bcec120e46dc23c7b
Kartikaya Gupta <kats@pancake.staktrace.com>
2021-03-27 12:55:00 -0400

    Avoid 'panic message is not a string literal' errors

--- ci-scripts/linux-release-tests.sh
+++ ci-scripts/linux-release-tests.sh
@@ -16,9 +16,9 @@ set -o xtrace
 CARGOFLAGS=${CARGOFLAGS:-""}  # default to empty if not set
 
 pushd wrench
-python script/headless.py reftest
-python script/headless.py rawtest
-python script/headless.py test_invalidation
+python3 script/headless.py reftest
+python3 script/headless.py rawtest
+python3 script/headless.py test_invalidation
 CXX=clang++ cargo run ${CARGOFLAGS} --release --features=software -- \
   --software --headless reftest
 popd
